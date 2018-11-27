3ecc515193e7f76366ff89a4c329697734da415f
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-11-27 03:38:52 +0300

    Лучшая ширина окна для непараметрической регрессии

--- regression/main.py
+++ regression/main.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plot
 import regression.kernels as kernels
 from regression.utils import makerange, makenoise
-from regression.nw_regression import nw_regression
+from regression.nw_regression import nw_regression, loo as nw_loo
 import numpy as np
 
-size = 200
+size = 100
 xl = makerange(-2 * np.pi, 4 * np.pi, size)
 yl = np.sin(xl) + makenoise(-0.2, 0.2, size)
 
@@ -13,13 +13,19 @@ plot.xlabel('x')
 plot.ylabel('sin(x)')
 plot.show()
 
-h = 0.5
-yl_r1 = list(map(lambda x: nw_regression(x, xl, yl, h, kernels.gauss), xl))
-yl_r2 = list(map(lambda x: nw_regression(x, xl, yl, h, kernels.quartic), xl))
+hl = np.arange(0.1, 1, 0.1)
+kernel = kernels.quartic
+
+h_errors = nw_loo(xl, yl, hl, kernel)
+print('H errors', h_errors)
+
+min_i = np.argmin(h_errors)
+h_error = round(h_errors[min_i], 5)
+best_h = round(hl[min_i], 1)
+yl_r = list(map(lambda x: nw_regression(x, xl, yl, best_h, kernel), xl))
 
 plot.plot(xl, yl)
-plot.plot(xl, yl_r1) # orange
-plot.plot(xl, yl_r2) # green
-plot.xlabel('x')
-plot.ylabel('sin(x)')
+plot.plot(xl, yl_r)
+plot.xlabel('x, h = ' + str(best_h))
+plot.ylabel('sin(x), mistake = ' + str(h_error))
 plot.show()
--- regression/nw_regression.py
+++ regression/nw_regression.py
@@ -1,10 +1,23 @@
 from regression.utils import distance
+import numpy as np
 
-def nw_regression(x, xl, y, h, kernel):
+def nw_regression(x, xl, yl, h, kernel):
     top, bottom = 0, 0
     for i, x_i in enumerate(xl):
         k = kernel(distance(x, x_i) / h)
-        top += y[i] * k
+        top += yl[i] * k
         bottom += k
 
-    return top / bottom
+    return float('inf') if bottom == 0 else top / bottom
+
+def loo(xl, yl, hl, kernel):
+    h_errors = []
+
+    for h in hl:
+        h_error = 0
+        for i, x in enumerate(xl):
+            y_r = nw_regression(x, np.delete(xl, i), np.delete(yl, i), h, kernel)
+            h_error += (y_r - yl[i]) ** 2
+        h_errors.append(h_error)
+
+    return h_errors
