10213a20c478e4dd937af6c217460be21f0ff265
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-12-24 00:30:21 +0300

    Интерактивные контролы

--- regression/datasets.py
+++ regression/datasets.py
@@ -4,6 +4,15 @@ from regression.utils import makerange, makenoise
 
 
 def sin_with_noise(lo, hi, size, noise=0.2):
+    return with_noise(np.sin, lo, hi, size, noise)
+
+def cos_with_noise(lo, hi, size, noise=0.2):
+    return with_noise(np.cos, lo, hi, size, noise)
+
+def ox_with_noise(lo, hi, size, noise=0.2):
+    return with_noise(lambda x: 0, lo, hi, size, noise)
+
+def with_noise(fn, lo, hi, size, noise=0.2):
     xl = makerange(lo, hi, size)
-    yl = np.sin(xl) + makenoise(-noise, noise, size)
+    yl = fn(xl) + makenoise(-noise, noise, size)
     return {'xl': xl, 'yl': yl}
--- regression/main.py
+++ regression/main.py
@@ -5,38 +5,44 @@ import regression.kernels as kernels
 import regression.datasets as datasets
 from regression.nw_regression import nw_regression, loo as nw_loo
 
-size = 100
-data = datasets.sin_with_noise(-2 * np.pi, 4 * np.pi, size, noise=0.3)
-xl, yl = data['xl'], data['yl']
-
-plot.plot(xl, yl)
-plot.xlabel('x')
-plot.ylabel('sin(x)')
-plot.show()
-
-def test_by_kernel(kernel, hl):
-    h_errors = nw_loo(xl, yl, hl, kernel)
-
-    min_i = np.argmin(h_errors)
-    h_error = round(h_errors[min_i], 5)
-    best_h = round(hl[min_i], 1)
-    yl_r = list(map(lambda x: nw_regression(x, xl, yl, best_h, kernel), xl))
+def test_nw_regression(size, fnName, noise):
+    data = getattr(datasets, fnName)(-2 * np.pi, 4 * np.pi, size, noise)
+    xl, yl = data['xl'], data['yl']
 
     plot.plot(xl, yl)
-    plot.plot(xl, yl_r)
-    plot.xlabel('x, h = ' + str(best_h))
-    plot.ylabel('sin(x), mistake = ' + str(h_error))
+    plot.xlabel('x')
+    plot.ylabel(fnName + '(x)')
     plot.show()
 
-    return h_errors
+    def test_by_kernel(kernel, kernelName, hl):
+        h_errors = nw_loo(xl, yl, hl, kernel)
 
-hl = np.arange(0.1, 1.5, 0.05)
-h_gauss_errors = test_by_kernel(kernels.gauss, hl)
-h_quartic_errors = test_by_kernel(kernels.quartic, hl)
+        min_i = np.argmin(h_errors)
+        h_error = round(h_errors[min_i], 5)
+        best_h = round(hl[min_i], 1)
+        yl_r = list(map(lambda x: nw_regression(x, xl, yl, best_h, kernel), xl))
 
-plot.plot(hl, h_gauss_errors)
-plot.plot(hl, h_quartic_errors)
-plot.legend(['gauss', 'quartic'])
-plot.xlabel('h')
-plot.ylabel('SSE')
-plot.show()
+        plot.plot(xl, yl)
+        plot.plot(xl, yl_r)
+        plot.xlabel('x, h = ' + str(best_h))
+        plot.ylabel(fnName + '(x), error = ' + str(h_error))
+        plot.title(kernelName)
+
+        return h_errors
+
+    hl = np.arange(0.1, 1.5, 0.05)
+
+    plot.figure()
+    plot.subplot(1, 2, 1)
+    h_gauss_errors = test_by_kernel(kernels.gauss, 'Gauss kernel', hl)
+
+    plot.subplot(1, 2, 2)
+    h_quartic_errors = test_by_kernel(kernels.quartic, 'Quartic kernel', hl)
+    plot.show()
+
+    plot.plot(hl, h_gauss_errors)
+    plot.plot(hl, h_quartic_errors)
+    plot.legend(['gauss', 'quartic'])
+    plot.xlabel('h')
+    plot.ylabel('SSE')
+    plot.show()
